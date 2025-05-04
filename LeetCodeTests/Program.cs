d91b461788da29ac2e3ab52f56736f993efed0e2
Babak Taremi <babak.taremi.dev@gmail.com>
2025-05-04 16:19:39 +0330

    Refactored code for readability and added LengthOfLastWord and IsPalindrome methods

--- LeetCodeTests/Program.cs
+++ LeetCodeTests/Program.cs
@@ -2,8 +2,7 @@
 
 Console.WriteLine("Hello, World!");
 
-
-
+var t=Solution.LengthOfLastWord("   fly me   to   the moon  ");
 
 public class Solution
 {
@@ -118,38 +117,37 @@ public class Solution
     {
         if (string.IsNullOrEmpty(s))
             return 0;
-        
+
         var result = 0;
         var sign = 1;
         var index = 0;
-        
+
         while (index < s.Length && s[index] == ' ')
             index++;
-        
+
         if (index < s.Length && (s[index] == '-' || s[index] == '+'))
         {
             sign = s[index] == '-' ? -1 : 1;
             index++;
         }
-        
+
         while (index < s.Length && char.IsDigit(s[index]))
         {
             var digit = s[index] - '0';
-        
+
             if (result > (int.MaxValue - digit) / 10)
                 return sign == 1 ? int.MaxValue : int.MinValue;
-        
+
             result = result * 10 + digit;
             index++;
         }
-        
+
         return result * sign;
-        
     }
-    
-    
-    public static string LongestCommonPrefix(string[] strs) {
-        
+
+
+    public static string LongestCommonPrefix(string[] strs)
+    {
         if (strs == null || strs.Length == 0)
             return "";
 
@@ -160,124 +158,143 @@ public class Solution
             while (strs[i].AsSpan().IndexOf(prefix) != 0)
             {
                 prefix = prefix.Slice(0, prefix.Length - 1);
-                if (prefix.Length==0)
+                if (prefix.Length == 0)
                     return "";
             }
         }
 
         return prefix.ToString();
     }
-    
-    public IList<IList<int>> ThreeSum(int[] nums) {
+
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
         Array.Sort(nums);
 
         IList<IList<int>> result = [];
-        
-        for (int i = 0; i < nums.Length - 2; i++) {
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
             if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicate i
-            
+
             int left = i + 1;
             int right = nums.Length - 1;
-            
-            while (left < right) {
+
+            while (left < right)
+            {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
+                if (sum == 0)
+                {
                     result.Add(new List<int> { nums[i], nums[left], nums[right] });
                     while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicate left
                     while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicate right
                     left++;
                     right--;
-                } else if (sum < 0) {
+                }
+                else if (sum < 0)
+                {
                     left++;
-                } else {
+                }
+                else
+                {
                     right--;
                 }
             }
         }
-        
+
         return result;
     }
-    
+
     public static int RemoveDuplicates(int[] nums)
     {
         if (nums.Length == 0) return 0;
-        
+
         int i = 0;
-        for (int j = 1; j < nums.Length; j++) {
-            if (nums[j] != nums[i]) {
+        for (int j = 1; j < nums.Length; j++)
+        {
+            if (nums[j] != nums[i])
+            {
                 i++;
                 nums[i] = nums[j];
             }
         }
+
         return i + 1;
     }
-    
-    public int SearchInsert(int[] nums, int target) 
+
+    public int SearchInsert(int[] nums, int target)
     {
         for (int i = 0; i < nums.Length; i++)
         {
-            if(nums[i]==target)
+            if (nums[i] == target)
                 return i;
-            
+
             if (nums[i] > target)
                 return i;
         }
-        
+
         return nums.Length;
     }
-    
-    public static int[] PlusOne(int[] digits) {
-        
-        for (int i = digits.Length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
+
+    public static int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
                 digits[i]++;
                 return digits;
             }
+
             digits[i] = 0;
         }
-        
+
         var result = new int[digits.Length + 1];
         result[0] = 1;
         return result;
     }
-    
-    public static IList<IList<string>> FindReversedPairs(string[] strs) {
+
+    public static IList<IList<string>> FindReversedPairs(string[] strs)
+    {
         IList<IList<string>> result = new List<IList<string>>();
         HashSet<string> seen = new HashSet<string>();
         HashSet<string> addedPairs = new HashSet<string>();
-        
-        foreach (string s in strs) {
+
+        foreach (string s in strs)
+        {
             seen.Add(s);
         }
-        
-        foreach (string s in strs) {
+
+        foreach (string s in strs)
+        {
             char[] charArray = s.ToCharArray();
             Array.Reverse(charArray);
             string reversed = new string(charArray);
-            
-            if (seen.Contains(reversed) && s != reversed && !addedPairs.Contains(reversed + s)) {
+
+            if (seen.Contains(reversed) && s != reversed && !addedPairs.Contains(reversed + s))
+            {
                 result.Add(new List<string> { s, reversed });
                 addedPairs.Add(s + reversed);
             }
         }
-        
+
         return result;
     }
-    
+
     public static int MaximumNumberOfStringPairs(string[] words)
     {
         int result = 0;
-      
+
         HashSet<string> addedPairs = new HashSet<string>();
-        
-        foreach (var s in words.AsSpan()) {
+
+        foreach (var s in words.AsSpan())
+        {
             char[] charArray = s.ToCharArray();
             Array.Reverse(charArray);
             string reversed = new string(charArray);
-            
-            if(s==reversed)
+
+            if (s == reversed)
                 continue;
-            
+
             if (words.Contains(reversed) && !addedPairs.Contains(reversed + s))
             {
                 result++;
@@ -286,9 +303,8 @@ public class Solution
         }
 
         return result;
-
     }
-    
+
     public static void MoveZeroes(int[] nums)
     {
         int lastNonZeroIndex = 0;
@@ -307,31 +323,127 @@ public class Solution
             nums[i] = 0;
         }
     }
-    
-    public static void MoveZeroesRuntimeOptimized(int[] nums) {
+
+    public static void MoveZeroesRuntimeOptimized(int[] nums)
+    {
         int lastNonZeroPos = 0;
-        for (int i = 0; i < nums.Length; i++) {
-            if (nums[i] != 0) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
                 // Swap nums[i] with nums[lastNonZeroPos]
                 (nums[i], nums[lastNonZeroPos]) = (nums[lastNonZeroPos], nums[i]);
                 lastNonZeroPos++;
             }
         }
     }
-    
-    public static void MoveZeroesAllOptimized(int[] nums) {
+
+    public static void MoveZeroesAllOptimized(int[] nums)
+    {
         int lastNonZeroIndex = 0;
-    
+
         // First pass: Move all non-zero elements to the front
-        for (int i = 0; i < nums.Length; i++) {
-            if (nums[i] != 0) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
                 nums[lastNonZeroIndex++] = nums[i];
             }
         }
-    
+
         // Second pass: Fill remaining positions with zeros (optimized)
-        while (lastNonZeroIndex < nums.Length) {
+        while (lastNonZeroIndex < nums.Length)
+        {
             nums[lastNonZeroIndex++] = 0;
         }
+    }
+
+    public static bool IsAcronym(IList<string> words, string s)
+    {
+            if (string.IsNullOrEmpty(s))
+                return false;
+
+            if (words.Count == 0)
+                return false;
+
+            if (words.Count != s.Length)
+                return false;
+
+            for(int i=0; i<s.Length ; i++)
+            {
+                if (words[i][0] != s[i])
+                    return false;
+            }
+
+            return true;
+    }
+    
+    public static bool IsPalindrome(int x)
+    {
+        // if (x < 0)
+        //     return false;
+        //
+        // var stringX= x.ToString();
+        //
+        // for (int i = 0; i < stringX.Length / 2; i++)
+        // {
+        //     if (stringX[i] != stringX[stringX.Length - 1 - i])
+        //         return false;
+        // }
+        //
+        // return true;
+
+
+        if (x < 0)
+            return false;
+        
+        var palindrome = 0;
+        var original = x;
+        
+        while (x>0)
+        {
+            var digit = x % 10;
+            palindrome= palindrome *10 + digit;
+            x /= 10;
+        }
+
+        return palindrome == original;
+    }
+    
+    
+    public static int RemoveElement(int[] nums, int val) {
+        
+        var j = 0;
+        
+        for (int i = 0; i <nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+    
+    public static int LengthOfLastWord(string s) {
+        
+        if (string.IsNullOrEmpty(s))
+            return 0;
+
+        var length = 0;
+        var i = s.Length - 1;
+
+        while (i >= 0 && s[i] == ' ')
+            i--;
+
+        while (i >= 0 && s[i] != ' ')
+        {
+            length++;
+            i--;
+        }
+
+        return length;
     }
 }
